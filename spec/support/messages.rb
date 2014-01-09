feb7d95b629a431c8dee78624e40997c48f73f37
Andrei <andrei@spreecommerce.com>
2014-01-09 09:51:45 -0500

    Adding user_updated message

--- spec/support/messages.rb
+++ spec/support/messages.rb
@@ -3,5 +3,9 @@ module Factories
     def user_new_payload
       JSON.parse IO.read("#{File.dirname(__FILE__)}/user_new.json")
     end
+
+    def user_updated_payload
+      JSON.parse IO.read("#{File.dirname(__FILE__)}/user_updated.json")
+    end
   end
 end
