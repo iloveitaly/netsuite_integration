c36f57de2635344b88fe94810806ea231f0ba9bb
Washington Luiz <huoxito@gmail.com>
2014-01-10 19:08:40 -0300

    Return any exception as a notification error

--- netsuite_endpoint.rb
+++ netsuite_endpoint.rb
@@ -5,31 +5,21 @@ require File.expand_path(File.dirname(__FILE__) + '/lib/netsuite_integration')
 
 class NetsuiteEndpoint < EndpointBase::Sinatra::Base
   post '/products' do
-    products = NetsuiteIntegration::Product.new(@config)
+    begin
+      products = NetsuiteIntegration::Product.new(@config)
 
-    if products.collection.any?
-      add_messages "product:import", products.messages
-      add_parameter 'netsuite.last_updated_after', products.last_modified_date
-      add_notification "info", "NetSuite Items imported as products up to #{products.last_modified_date}"
-    else
-      add_notification "info", "No product updated since #{@config.fetch('netsuite.last_updated_after')}"
+      if products.collection.any?
+        add_messages "product:import", products.messages
+        add_parameter 'netsuite.last_updated_after', products.last_modified_date
+        add_notification "info", "NetSuite Items imported as products up to #{products.last_modified_date}"
+      else
+        add_notification "info", "No product updated since #{@config.fetch('netsuite.last_updated_after')}"
+      end
+
+      process_result 200
+    rescue Exception => e
+      add_notification "error", e.message, nil, { backtrace: e.backtrace.to_a.join("\n\t") }
+      process_result 500
     end
-
-    process_result 200
-  end
-
-  private
-  def error_notification(e)
-    {
-      'message_id' => @message_id,
-      'notifications' => [
-        {
-          "level" => "error",
-          "subject" => e.message,
-          "description" => e.message,
-          "backtrace" => e.backtrace.to_a.join("\n\t")
-        }
-      ]
-    }
   end
 end
