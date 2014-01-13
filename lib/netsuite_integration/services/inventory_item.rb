28e8dd6b97851ccbc548eab73a7e5246a65aeeab
Washington Luiz <huoxito@gmail.com>
2014-01-13 15:08:35 -0300

    Bring Inventory Items only in search

--- lib/netsuite_integration/services/inventory_item.rb
+++ lib/netsuite_integration/services/inventory_item.rb
@@ -6,9 +6,8 @@ module NetsuiteIntegration
     # In order to retrieve a Matrix Item you also need to enable "Matrix Items"
     # in your Company settings
     #
-    # Fetch record between two dates to avoid crazy future modified dates in
-    # NetSuite records returned by api. e.g. Say today is Jan 8. We might see
-    # products updated in Jan 31 (WTF).
+    # Specify Item type because +search+ maps to NetSuite ItemSearch object
+    # which will bring all kinds of items and not only inventory items
     #
     # Records need to be ordered by lastModifiedDate programatically since
     # NetSuite api doesn't allow us to do that on the request. That's the
@@ -16,8 +15,6 @@ module NetsuiteIntegration
     # catch all items at once and sort by date properly or we might end up
     # losing data
     class InventoryItem < Base
-      # TODO Select only items which date is after today
-      # TODO Run script to update all created or modified after current Date
       def latest
         search.sort_by { |c| c.last_modified_date.utc }
       end
@@ -33,12 +30,18 @@ module NetsuiteIntegration
                   value: last_updated_after
                 },
                 {
+                  field: 'type',
+                  operator: 'anyOf',
+                  type: 'SearchEnumMultiSelectField',
+                  value: ['_inventoryItem']
+                },
+                {
                   field: 'isInactive',
                   value: false
                 }
               ]
-            }
-            # TODO we will probably still need this to fetch matrix options
+            },
+            # # TODO we will probably still need this to fetch matrix options
             # preferences: {
             #   'bodyFieldsOnly' => false
             # }
