6667376c2f970f3ac159e1580d7924b9d1c3d178
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-01-16 11:32:25 -0200

    Adds shipment:confirm message, refactors factory

--- spec/support/messages.rb
+++ spec/support/messages.rb
@@ -1,15 +1,9 @@
 module Factories
   class << self
-    def user_new_payload
-      JSON.parse IO.read("#{File.dirname(__FILE__)}/user_new.json")
-    end
-
-    def user_updated_payload
-      JSON.parse IO.read("#{File.dirname(__FILE__)}/user_updated.json")
-    end
-
-    def order_new_payload
-      JSON.parse IO.read("#{File.dirname(__FILE__)}/order_new.json")
+    [:user_new, :user_updated, :order_new, :shipment_confirm].each do |message|
+      define_method("#{message}_payload") do
+        JSON.parse IO.read("#{File.dirname(__FILE__)}/#{message}.json")
+      end
     end
   end
 end
