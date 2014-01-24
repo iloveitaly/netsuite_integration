fd12d8680e8f483d21ff6e469ddf8547db37a606
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-01-24 15:54:24 -0200

    Handles errors on creating invoices, item fulfillments

--- lib/netsuite_integration/shipment.rb
+++ lib/netsuite_integration/shipment.rb
@@ -1,25 +1,30 @@
 module NetsuiteIntegration
   class Shipment < Base
-    attr_reader :config, :collection, :order
+    attr_reader :config, :collection
 
     def import
-      raise "Can't create an item fulfillment for #{order.external_id}" unless create_item_fulfillment!
-      raise "Can't create an invoice for #{order.external_id}" unless create_invoice!
+      create_item_fulfillment
+      create_invoice
+
       order
     end
 
-    def create_invoice!
+    def create_invoice
+      return unless order_pending_billing?
+
       invoice = NetSuite::Records::Invoice.new({
-        tax_total: 0,
         created_from: {
           internal_id: order_id
         }
       })
 
       invoice.add
+      verify_errors(invoice)
     end
 
-    def create_item_fulfillment!
+    def create_item_fulfillment
+      return unless order_pending_fulfillment?
+
       fulfillment = NetSuite::Records::ItemFulfillment.new({
         created_from: {
           internal_id: order_id
@@ -37,16 +42,36 @@ module NetsuiteIntegration
       })
 
       fulfillment.add
+      verify_errors(fulfillment)
     end
 
     private
-    def  order_id
+    def order_pending_fulfillment?
+      order.status == 'Pending Fulfillment'
+    end
+
+    def order_pending_billing?
+      @fulfilled || order.status == 'Pending Billing'
+    end
+
+    def order_id
+      order.internal_id
+    end
+
+    def order
       @order ||= sales_order_service.find_by_external_id(payload[:shipment][:order_number])
-      @order.internal_id
     end
 
     def address
       payload[:shipment][:shipping_address]
+    end
+
+    def verify_errors(object)
+      if errors = (object.errors || []).select {|e| e.type == "ERROR"}
+        text = errors.inject("") {|buf, cur| buf += cur.message}
+
+        raise Exception.new(text) if text.length > 0
+      end
     end
   end
 end
--- spec/netsuite_integration/shipment_spec.rb
+++ spec/netsuite_integration/shipment_spec.rb
@@ -19,5 +19,26 @@ module NetsuiteIntegration
         end
       end
     end
+
+    context 'when order has already been fulfilled' do
+      context 'when invoice is ok' do
+        xit 'creates only the invoice' do
+          VCR.use_cassette("shipment/import_only_invoice") do
+            fulfilled_order = subject.import
+
+            fulfilled_order.internal_id.should eq("9593")
+            fulfilled_order.external_id.should eq("R375526411")
+          end
+        end
+      end
+
+      context 'when invoice has errors' do
+        it 'generates an error' do
+          VCR.use_cassette("shipment/order_fulfilled_but_errors_on_invoice") do
+            expect { subject.import }.to raise_error("Transaction is not in balance!  amounts+taxes+shipping: 194.0, total amount: 208.77")
+          end
+        end
+      end
+    end
   end
 end
