db80ca08f033ad881a651cc58bea00f272f2ba96
Washington Luiz <huoxito@gmail.com>
2014-03-28 19:11:43 -0300

    Update parameteres name format

--- spec/support/shared_examples/config.rb
+++ spec/support/shared_examples/config.rb
@@ -1,18 +1,18 @@
 config_hash = {
-  'netsuite.api_version' => '2013_2',
-  'netsuite.wsdl_url' => 'https://webservices.na1.netsuite.com/wsdl/v2013_2_0/netsuite.wsdl',
-  'netsuite.email' => 'washington@spreecommerce.com',
-  'netsuite.password' => 'test',
-  'netsuite.account' => 'test',
-  'netsuite.last_updated_after' => '2012-01-08T18:48:56.001Z',
-  'netsuite.account_for_sales_id' => 2,
-  'netsuite.shipping_methods_mapping' => [{
+  'netsuite_api_version' => '2013_2',
+  'netsuite_wsdl_url' => 'https://webservices.na1.netsuite.com/wsdl/v2013_2_0/netsuite.wsdl',
+  'netsuite_email' => 'washington@spreecommerce.com',
+  'netsuite_password' => 'test',
+  'netsuite_account' => 'test',
+  'netsuite_last_updated_after' => '2012-01-08T18:48:56.001Z',
+  'netsuite_account_for_sales_id' => 2,
+  'netsuite_shipping_methods_mapping' => [{
     "UPS Ground (USD)"=>"92",
     "UPS Two Day (USD)"=>"91",
     "UPS One Day (USD)"=>"77",
     "UPS"=>"77"
   }],
-  'netsuite.payment_methods_mapping' => [{
+  'netsuite_payment_methods_mapping' => [{
     "Credit Card" => "5",
     "Cash" => "1"
   }]  
@@ -24,17 +24,17 @@ end
 
 shared_context "request parameters" do
   let(:parameters) do
-    [
-      {:name => 'netsuite.api_version', :value => "2013_2" },
-      {:name => 'netsuite.wsdl_url', :value => "https://webservices.na1.netsuite.com/wsdl/v2013_2_0/netsuite.wsdl" },
-      {:name => "netsuite.email", :value => "washington@spreecommerce.com" },
-      {:name => "netsuite.password", :value => "test" },
-      {:name => "netsuite.account", :value => "test" },
-      {:name => "netsuite.last_updated_after", :value => "2013-01-08T18:48:56.001Z" },
-      {:name => "netsuite.account_for_sales_id", :value => "2"},
-      {:name => "netsuite.shipping_methods_mapping", :value => [{ "UPS" => "77", "UPS Ground (USD)"=>"92", "UPS Two Day (USD)"=>"91", "UPS One Day (USD)"=>"77" }]},
-      {:name => "netsuite.payment_methods_mapping", :value => [{ "Credit Card" => "5", "Cash" => "1" }]}
-    ]
+    {
+      'netsuite_api_version' => "2013_2",
+      'netsuite_wsdl_url' => "https://webservices.na1.netsuite.com/wsdl/v2013_2_0/netsuite.wsdl",
+      "netsuite_email" => "washington@spreecommerce.com",
+      "netsuite_password" => "test",
+      "netsuite_account" => "test",
+      "netsuite_last_updated_after" => "2013-01-08T18:48:56.001Z",
+      "netsuite_account_for_sales_id" => "2",
+      "netsuite_shipping_methods_mapping" => [{ "UPS" => "77", "UPS Ground (USD)"=>"92", "UPS Two Day (USD)"=>"91", "UPS One Day (USD)"=>"77" }],
+      "netsuite_payment_methods_mapping" => [{ "Credit Card" => "5", "Cash" => "1" }]
+    }
   end
 end
 
@@ -42,12 +42,12 @@ shared_context "connect to netsuite" do
   before(:all) do
     NetSuite.configure do
       reset!
-      api_version config_hash.fetch('netsuite.api_version')
-      wsdl        config_hash.fetch('netsuite.wsdl_url')
+      api_version config_hash.fetch('netsuite_api_version')
+      wsdl        config_hash.fetch('netsuite_wsdl_url')
       sandbox     false
-      email       config_hash.fetch('netsuite.email')
-      password    config_hash.fetch('netsuite.password')
-      account     config_hash.fetch('netsuite.account')
+      email       config_hash.fetch('netsuite_email')
+      password    config_hash.fetch('netsuite_password')
+      account     config_hash.fetch('netsuite_account')
       log_level   :info
     end
   end
