68ae7c7b47c5a13ebc55d842339fad619f308f8a
Washington Luiz <huoxito@gmail.com>
2014-03-31 15:56:35 -0300

    No need to have two helpers hash for parameteres anymore

--- spec/support/shared_examples/config.rb
+++ spec/support/shared_examples/config.rb
@@ -23,19 +23,7 @@ shared_examples "config hash" do
 end
 
 shared_context "request parameters" do
-  let(:parameters) do
-    {
-      'netsuite_api_version' => "2013_2",
-      'netsuite_wsdl_url' => "https://webservices.na1.netsuite.com/wsdl/v2013_2_0/netsuite.wsdl",
-      "netsuite_email" => "washington@spreecommerce.com",
-      "netsuite_password" => "test",
-      "netsuite_account" => "test",
-      "netsuite_last_updated_after" => "2013-01-08T18:48:56.001Z",
-      "netsuite_account_for_sales_id" => "2",
-      "netsuite_shipping_methods_mapping" => [{ "UPS" => "77", "UPS Ground (USD)"=>"92", "UPS Two Day (USD)"=>"91", "UPS One Day (USD)"=>"77" }],
-      "netsuite_payment_methods_mapping" => [{ "Credit Card" => "5", "Cash" => "1" }]
-    }
-  end
+  let(:parameters) { config_hash }
 end
 
 shared_context "connect to netsuite" do
