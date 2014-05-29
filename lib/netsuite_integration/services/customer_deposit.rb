fa1e8e316342d17dbdd64ad7cfbffa206c53a938
Washington Luiz <huoxito@gmail.com>
2014-05-29 11:46:31 -0300

    Set tran_date and undep_funds as needed for Deposit
    Some NetSuite instances have a constraint on the deposit transaction
    date not being too far away from the sales order transaction.
    
    Also undeposited funds accout might not be required for all instances

--- lib/netsuite_integration/services/customer_deposit.rb
+++ lib/netsuite_integration/services/customer_deposit.rb
@@ -1,10 +1,11 @@
 module NetsuiteIntegration
   module Services
     class CustomerDeposit < Base
-      attr_reader :payments, :persisted
+      attr_reader :payments, :persisted, :payload
 
       def initialize(config, payload = {})
         super config
+        @payload = payload
         @payments = payload[:order][:payments] if payload[:order]
       end
 
@@ -15,7 +16,6 @@ module NetsuiteIntegration
           if payment[:amount] > 0
             unless deposit = find_by_external_id(external_id)
               deposit = build(sales_order, payment)
-
               result = deposit.add
               
               # Need to know if at least one of them was persisted
@@ -37,16 +37,21 @@ module NetsuiteIntegration
 
         # Setting external_id to Spree's order number, so we could search by it later
         # Warning: external_id must be unique across all NetSuite data objects
-        # TODO Revisit searching for customer deposits for a specific sales order
         deposit.external_id = "#{prefix}-#{sales_order.external_id}-#{payment[:number]}"
 
         deposit.sales_order = NetSuite::Records::RecordRef.new(internal_id: sales_order.internal_id)
         deposit.customer = NetSuite::Records::RecordRef.new(internal_id: sales_order.entity.internal_id)
-        # TODO check for reference error between customer and account
-        deposit.account = NetSuite::Records::RecordRef.new(internal_id: config.fetch('netsuite_account_for_sales_id'))
-        # possible permission error on the accounts field if this is not set to false
-        deposit.undep_funds = false
+
+        if config['netsuite_account_for_sales_id'].present?
+          deposit.account = NetSuite::Records::RecordRef.new(internal_id: config.fetch('netsuite_account_for_sales_id'))
+          # possible permission error on the accounts field if this is not set to false
+          deposit.undep_funds = false
+        else
+          deposit.undep_funds = true
+        end
+
         deposit.payment = payment[:amount]
+        deposit.tran_date = sales_order.tran_date || payload[:order][:placed_on]
 
         deposit
       end
