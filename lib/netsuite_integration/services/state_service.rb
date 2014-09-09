077517c2f363240630301f31b780057f5f452cad
Washington Luiz <huoxito@gmail.com>
2014-09-09 08:48:01 -0300

    Revert "Revert "Grab states from a file""
    This reverts commit 9f93fd02c5b2566d51c7a34f97c55ccd21a99547.

--- lib/netsuite_integration/services/state_service.rb
+++ lib/netsuite_integration/services/state_service.rb
@@ -6,67 +6,17 @@ module NetsuiteIntegration
           # NOTE Assume that a valid two digit US state was given
           return state_name if state_name.to_s.size == 2
 
-          if abbr = @@states[state_name]
+          if abbr = @@states[state_name.to_s]
             abbr
           else
             all_states[state_name] || state_name
           end
         end
 
-        @@states = {
-          "Alabama" => "AL",
-          "Alaska" => "AK",
-          "Arizona" => "AZ",
-          "Arkansas" => "AR",
-          "California" => "CA",
-          "Colorado" => "CO",
-          "Connecticut" => "CT",
-          "District of Columbia" => "DC",
-          "Delaware" => "DE",
-          "Florida" => "FL",
-          "Georgia" => "GA",
-          "Hawaii" => "HI",
-          "Idaho" => "ID",
-          "Illinois" => "IL",
-          "Indiana" => "IN",
-          "Iowa" => "IA",
-          "Kansas" => "KS",
-          "Kentucky" => "KY",
-          "Louisiana" => "LA",
-          "Maine" => "ME",
-          "Maryland" => "MD",
-          "Massachusetts" => "MA",
-          "Michigan" => "MI",
-          "Minnesota" => "MN",
-          "Mississippi" => "MS",
-          "Missouri" => "MO",
-          "Montana" => "MT",
-          "Nebraska" => "NE",
-          "Nevada" => "NV",
-          "New Hampshire" => "NH",
-          "New Jersey" => "NJ",
-          "New Mexico" => "NM",
-          "New York" => "NY",
-          "North Carolina" => "NC",
-          "North Dakota" => "ND",
-          "Ohio" => "OH",
-          "Oklahoma" => "OK",
-          "Oregon" => "OR",
-          "Pennsylvania" => "PA",
-          "Rhode Island" => "RI",
-          "South Carolina" => "SC",
-          "South Dakota" => "SD",
-          "Tennessee" => "TN",
-          "Texas" => "TX",
-          "Utah" => "UT",
-          "Vermont" => "VT",
-          "Virginia" => "VA",
-          "Washington" => "WA",
-          "West Virginia" => "WV",
-          "Wisconsin" => "WI",
-          "Wyoming" => "WY"
-        }
+        @@states = JSON.parse IO.read(File.join(__dir__, "../../netsuite/states.json"))
 
+        # NOTE Ideally this should never run live. All possible states should
+        # be in netsuite/states.json
         def all_states
           states = NetSuite::Configuration.connection.call(:get_all, message: {
             'platformCore:record' => {
